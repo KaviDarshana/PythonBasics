9701cbdf074fb74acda8d25baa5dd551b49c9640
KaviDarshana <152808959+KaviDarshana@users.noreply.github.com>
2025-08-21 12:21:14 +0530

    Add files via upload
    Added 4 files, which were used to learn file handling in Python. (open.uom.lk tutorial)

---
+++ Python/File_Handling.py
@@ -0,0 +1,38 @@
+# fhandle = open("file.txt")
+# fhandle = open("file.txt", "r")
+
+# fhandle = open("file.txt")
+# fcontents = fhandle.read()
+# print(fcontents)
+# fhandle.close()
+
+
+# fhandle = open("file.txt")
+# print(fhandle.read())
+
+
+# fhandle = open("file.txt")
+# print(fhandle.readline())
+# print(fhandle.readline())
+# print(fhandle.readline())
+# print(fhandle.readline())
+# print(fhandle.readline())
+# fhandle.close()
+
+# fhandle = open("file.txt", "w")
+# newText = "This text is written to test the write method in Python. \n File handling is essential for data management and it is easy to carry out with Python."
+# fhandle.write(newText)
+# fhandle.close()
+
+# fhandle = open("file.txt")
+# print(fhandle.read())
+# fhandle.close()
+
+
+# open("myfile.txt")
+# open("myfile.txt", "r")
+# open("myfile.txt", "w")
+# open("testFileCreation.py","w")
+
+fhandle = open("C:/users/darsh/OneDrive/Desktop/Tasks.txt", "r")
+print(fhandle.read())
